6eac94244f7b9e1efbf1d2ee818606828aef48eb
nguyenhung98bk <nguyenviethung98.bk@gmail.com>
2020-06-14 14:46:44 +0700

    create forder purpose

---
+++ front_end/src/layouts/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <footer class="main-footer">
+      <div class="float-right d-none d-sm-block">
+        <b>Version</b> 1.0.1
+      </div>
+      <strong>
+        Copyright &copy; 2020
+        <a href="#">VDN</a>.
+      </strong>
+    </footer>
+
+    
+  </div>
+</template>
+
+<style>
+</style>
